de961faaac9ec07b5affe55296bbd0fa4dcb3f9e
FarDean <sfh1212@gmail.com>
2021-05-12 11:47:19 +0430

    changed favicon

--- template.js
+++ template.js
@@ -1,7 +1,8 @@
-module.exports=(markup,helmet)=>{
-    return `
+module.exports = (markup, helmet) => {
+	return `
     <!DOCTYPE html>
     <html lang="en" ${helmet.htmlAttributes.toString()}>
+
     <head>
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
@@ -9,6 +10,7 @@ module.exports=(markup,helmet)=>{
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="icon" type="image/png" href="https://img.icons8.com/nolan/50/google-blog-search.png" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     </head>
@@ -17,5 +19,5 @@ module.exports=(markup,helmet)=>{
         <script type="text/javascript" src="/dist/bundle.js"></script>
     </body>
     </html>
-    `
-}
+    `;
+};
