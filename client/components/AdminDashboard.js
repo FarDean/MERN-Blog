0a8fa74b2c99e383b4d90c1dbebfc4ee860d1608
FarDean <sfh1212@gmail.com>
2021-05-12 11:19:40 +0430

    react helmet implemented 2

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 export default function AdminDashboard() {
-    return (
-            <div className="container">
-                <div className="dashboard">
-                <Link to='/newarticle'>Create los new Article </Link>
-                </div>
-            </div>
-    )
+	return (
+		<div className="container">
+			<Helmet>
+				<title>Admin Dashboard - FarDean Blog</title>
+			</Helmet>
+			<div className="dashboard">
+				<Link to="/newarticle">Create new Article </Link>
+			</div>
+		</div>
+	);
 }
