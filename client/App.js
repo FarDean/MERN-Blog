10ef6a603569e7d7ce96e4239aa91323a47243e9
FarDean <sfh1212@gmail.com>
2021-05-12 11:24:00 +0430

    scroll to top component added

--- client/App.js
+++ client/App.js
@@ -1,21 +1,22 @@
-import { hot } from 'react-hot-loader/root';
-import React from 'react'
-import { BrowserRouter,withRouter } from 'react-router-dom';
-import MainRouter from './MainRouter';
-import './assets/css/App.css'
-import './../node_modules/slick-carousel/slick/slick.css'
+import { hot } from "react-hot-loader/root";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./MainRouter";
+import "./assets/css/App.css";
+import "./../node_modules/slick-carousel/slick/slick.css";
 import "./../node_modules/slick-carousel/slick/slick-theme.css";
-import {GlobalProvider} from './context/GlobalContext'
+import { GlobalProvider } from "./context/GlobalContext";
+import ScrollToTop from "./components/utils/ScrollToTop";
 
+const App = () => {
+	return (
+		<GlobalProvider>
+			<BrowserRouter>
+				<ScrollToTop />
+				<MainRouter />
+			</BrowserRouter>
+		</GlobalProvider>
+	);
+};
 
-const App =()=> {
-    return (
-        <GlobalProvider>
-            <BrowserRouter>
-                <MainRouter />
-            </BrowserRouter>
-        </GlobalProvider>
-    )
-}
-
-export default hot(App) 
+export default hot(App);
